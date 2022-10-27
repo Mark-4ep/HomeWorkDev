f012386e53ea3cbb956e3899554e6b84c5765d6e
Mark <62542962+Mark-4ep@users.noreply.github.com>
2022-10-27 16:07:53 +0300

    task completed

---
+++ HomeWrok3/find_longest_project.sql
@@ -0,0 +1,7 @@
+SELECT id as 'project_id',
+  TIMESTAMPDIFF(MONTH, start_date, finish_date)  AS 'month_count'
+FROM project
+WHERE
+TIMESTAMPDIFF(MONTH, start_date, finish_date) = (SELECT MAX(TIMESTAMPDIFF(MONTH, start_date, finish_date))
+FROM project)
+;
---
+++ HomeWrok3/find_youngest_eldest_workers.sql
@@ -0,0 +1,14 @@
+SELECT
+  'ELDEST' AS 'Type',
+   name,
+   birthday
+FROM worker
+WHERE  birthday = (SELECT MIN(birthday)  FROM worker)
+UNION
+SELECT
+   'YOUNGEST' AS 'Type',
+    name,
+    birthday
+FROM worker
+WHERE birthday = (SELECT MAX(birthday) FROM worker)
+;
